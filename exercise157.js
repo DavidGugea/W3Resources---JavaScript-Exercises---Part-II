898d1b33baf6c62b4b4020fef2d3f8a8bf023019
David Ioannis Gugea <d.gugea@wms-hn.org>
2020-03-02 11:02:20 +0100

    Exercise done.

---
+++ exercise157.js
@@ -0,0 +1,5 @@
+// 157. Write a JavaScript program to redirect to a specified URL.
+
+let redirectToUrl = (redirectUrl) => document.location = redirectUrl;
+
+redirectToUrl("https://www.google.com")
