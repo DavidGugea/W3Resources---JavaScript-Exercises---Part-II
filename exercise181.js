a9d12843ab974d8cbb895ad6323df1fe35c518ce
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-03 22:16:40 +0100

    Exercise done.

---
+++ exercise181.js
@@ -0,0 +1,5 @@
+// 181. Write a JavaScript program to get the last element from an given array. 
+
+const getLast = (arr) => arr[arr.length-1]
+let myArray = [1, 2, 3];
+console.log(getLast(myArray))
