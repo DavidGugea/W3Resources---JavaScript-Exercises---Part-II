8f815518d98b73a5cfcc2ac2eeb4d98eec5143c8
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-03 20:46:02 +0100

    Exercise done.

--- exercise169.js
+++ exercise169.js
@@ -1,6 +1,6 @@
 // 169. Write a JavaScript program to create a function that invokes fn with partials appended to the arguments it receives.
 
-const partialApplication = (fn, ...args0) => (...args1) => fn(...args0, ...args1);
+const partialApplication = (fn, ...args0) => (...args1) => fn(...args1, ...args0);
 const addThreeNumbers = (a, b, c) => a + b + c;
 
 console.log(partialApplication(addThreeNumbers, 1)(2, 3));
