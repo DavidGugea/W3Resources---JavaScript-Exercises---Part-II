bc6fdc7dd6304d47fd3060e07c9b07981a93412d
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 18:42:23 +0100

    Exercise done.

---
+++ exercise197.js
@@ -0,0 +1,7 @@
+// 197. Write a JavaScript program to check if a string is lower case or not.
+
+const isStringLowerCase = (string) => string.toLowerCase() == string
+
+console.log(isStringLowerCase("asd"));
+console.log(isStringLowerCase("ASD"));
+console.log(isStringLowerCase("aSd"));
