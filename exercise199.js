ad0d0f5da3b4f45f3d2da8277357df0186e45b05
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 18:45:42 +0100

    Exercise done.

---
+++ exercise199.js
@@ -0,0 +1,6 @@
+// 199. Write a JavaScript program that will return true if the given number is even, false otherwise.
+
+const isEven = (num) => num % 2 == 0
+
+console.log(isEven(4));
+console.log(isEven(5));
