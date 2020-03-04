f9a9dc401c647f37187499e19d74f11dfa5b939a
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 12:07:23 +0100

    Exercise done.

---
+++ exercise184.js
@@ -0,0 +1,7 @@
+// 184. Write a JavaScript program to check if a given string is upper case or not.
+
+const isUppercase = (str) => str.toUpperCase() == str;
+
+console.log(isUppercase("ASD"));
+console.log(isUppercase("asd"));
+console.log(isUppercase("aSd"));
