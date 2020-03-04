89249e7b533974ef93434a7ecd831fe25209f8b1
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 17:02:49 +0100

    Exercise done.

---
+++ exercise187.js
@@ -0,0 +1,6 @@
+// 187. Write a JavaScript program to check if the given argument is a string.
+
+const isString = (value) => typeof value == "string"
+
+console.log(isString("asd"));
+console.log(isString(12));
