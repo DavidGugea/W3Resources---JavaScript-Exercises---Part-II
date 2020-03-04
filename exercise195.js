a59375a108863b5d6b6a411edd82fedcd52c4ff0
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 18:35:27 +0100

    Exercise done.

---
+++ exercise195.js
@@ -0,0 +1,6 @@
+// 195. Write a JavaScript program to check if a given argument is a number.
+
+const isNumber = (value) => typeof value === "number"
+
+console.log(isNumber(2));
+console.log(isNumber("asd"));
