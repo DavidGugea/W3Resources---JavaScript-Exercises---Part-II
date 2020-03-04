c5ec3bbeb5c5dcb7080fe1e60595612202263f51
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 12:37:01 +0100

    Exercise done.

---
+++ exercise185.js
@@ -0,0 +1,5 @@
+// 185. Write a JavaScript program that will return true if the specified value is undefined, false otherwise. 
+
+const isUndefined = (value) => value === undefined
+console.log(isUndefined(undefined));
+console.log(isUndefined(2));
