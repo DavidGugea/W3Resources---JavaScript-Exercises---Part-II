b7e73f547498dcc7cc4d6c096f7e93861a72d07a
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-04 18:51:18 +0100

    Exercise done.

---
+++ exercise201.js
@@ -0,0 +1,7 @@
+// 201. Write a JavaScript program to check if the first numeric argument is divisible by the second one.
+
+const areDivisible = (i, j) => i % j == 0
+
+console.log(areDivisible(4, 2));
+console.log(areDivisible(8, 4));
+console.log(areDivisible(9, 7));
