1a20ff15c05482835f0cb42576d688d483a34e01
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-03-06 21:07:54 +0100

    Exercise done.

---
+++ exercise246.js
@@ -0,0 +1,5 @@
+// 246. Write a JavaScript program to convert an given angle from degrees to radians. 
+
+const convertDegreesToRadians = (degree) => degree *  Math.PI / 180
+console.log(convertDegreesToRadians(90.0));
+console.log(convertDegreesToRadians(30.0));
